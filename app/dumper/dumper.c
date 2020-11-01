df6fe862156b9f2b2e7ac2fb9547509a1d324ab4
Christian Speckner <christian.speckner@mayflower.de>
2020-11-01 23:58:30 +0000

    Rework dumper, add option for dumping to SD.

--- app/dumper/dumper.c
+++ app/dumper/dumper.c
@@ -3,7 +3,23 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
-#include <string.h> // needed for memset
+#include <signal.h>
+#include <string.h>
+#include <wait.h>
+
+enum State {
+	STATE_NO_STELLA,
+	STATE_STELLA_WITHOUT_ROM,
+	STATE_STELLA_WITH_ROM
+};
+
+const unsigned char status_cmd[] = { 0xa9, 0x02, 0x80, 0x00, 0x00 };//request status
+const unsigned char send_cmd[] =   { 0xa9, 0x02, 0x40, 0x00, 0x00 };//request send data
+const unsigned char reset_cmd[] =  { 0xa9, 0x02, 0x20, 0x00, 0x00 };//request reset
+const char* pathToStella = "/usr/bin/stella";
+
+enum State state = STATE_NO_STELLA;
+pid_t stellaPid;
 int tty_fd = 0;
 
 int ReadMCU(unsigned char *buf, int len){
@@ -41,7 +57,6 @@ void OpenMCU(){
 	tty_fd = open("/dev/ttyS2", O_RDWR);
 	if(tty_fd < 0) {printf("OpenMCU Failed\n"); return; }
 
-
 	cfsetispeed(&options, B115200); //set input baud rates speed
 	cfsetospeed(&options, B115200); //set output baud rates speed
 	options.c_cflag |= (CS8 | CREAD | CLOCAL);
@@ -55,107 +70,142 @@ void OpenMCU(){
 	return ;
 }
 
+void runStella(const char* romfile) {
+	if (!romfile && state == STATE_STELLA_WITHOUT_ROM) return;
+
+	if (state != STATE_NO_STELLA) {
+		printf("killing stella...\n");
+
+		kill(stellaPid, SIGTERM);
+
+		int exitStatus;
+		waitpid(stellaPid, &exitStatus, 0);
+	}
+
+	pid_t pid = fork();
+
+	if (pid < 0) {
+		fprintf(stderr, "failed to fork\n");
+		state = STATE_NO_STELLA;
+	} else if (pid == 0) {
+		printf("launching stella...\n");
+
+		romfile ? execlp(pathToStella, pathToStella, romfile, NULL) : execlp(pathToStella, pathToStella, NULL);
+
+		fprintf(stderr, "failed to execute stella\n");
+		exit(0);
+	} else {
+		stellaPid = pid;
+		state = romfile ? STATE_STELLA_WITH_ROM : STATE_STELLA_WITHOUT_ROM;
+	}
+}
 
 int main(int argc, char** argv){
+	if (argc != 2) {
+		fprintf(stderr, "usage: dumper <romfile>\n");
+		exit(1);
+	}
+
+	const char* romfile = argv[1];
+
 	int cartridge_len = 4 * 1024;
 	int ret = 0;
-//        char cmd_to_run[] = "/usr/bin/killall stella; /usr/bin/killall r77_front_end; /bin/rm /dev/rom.bin; /bin/sleep 2; /usr/bin/r77_front_end&";
-		char cmd_to_run[] = "/usr/bin/killall stella; /bin/rm /dev/rom.bin; /bin/sleep 2; /usr/bin/stella&";
-        char cmd_to_run_factory[] = "/usr/bin/factory&";
-	unsigned char status_cmd[] = { 0xa9, 0x02, 0x80, 0x00, 0x00 };//request status
-	unsigned char send_cmd[] =   { 0xa9, 0x02, 0x40, 0x00, 0x00 };//request send data
-	unsigned char reset_cmd[] =  { 0xa9, 0x02, 0x20, 0x00, 0x00 };//request reset
+
 	unsigned char buf[128 * 1024];
-	int factory_mode = 0;
 
 	printf("Atari Dumper\n");
 	OpenMCU();
 	memset(buf, 0, 128 * 1024);
 
-	if(access("/usr/bin/factory", F_OK) != -1){
-		factory_mode = 1;
-		system(cmd_to_run_factory);
-	}else{
-		factory_mode = 0;
-	}
-
 	write(tty_fd, status_cmd, 4); //send read status, for first time booting
-	while(1){
+
+	while (1)
+	{
 		memset(buf, 0, 128 * 1024);
 		read(tty_fd, buf, 4);//MCU will auto generate status data when plug/unplug
 
 		if(*(buf + 2) == 0){
 			printf("No Cartridge\n");
-			if(factory_mode == 0)
-	                        system(cmd_to_run);
-			else
-				system("/bin/rm -rf /dev/rom.bin");
-		}else if(*(buf + 2) == 1){
-			printf("Cartridge Type: ");
-			switch(*(buf + 3)){
-				case 0:
-					cartridge_len = 4 * 1024;
-					printf(" 4K\n");
-					break;
-				case 1:
-					cartridge_len = 8 * 1024;
-					printf(" 8K\n");
-					break;
-				case 2:
-					cartridge_len = 16 * 1024;
-					printf("16K\n");
-					break;
-				case 3:
-					cartridge_len = 32 * 1024;
-					printf("32K\n");
-					break;
-				default://can not get correct type
-					if(factory_mode == 0)
-						system(cmd_to_run);
-					else
-						system("/bin/rm -rf /dev/rom.bin");
-                                        continue;
-					break;
-			}
-			int receive_count = 0, buf_index = 0, write_count = 0;
-			unsigned char* buf2 = buf;
-			FILE* rom = fopen("/dev/rom.bin", "wb"); 
 
-			memset(buf, 0, 128 * 1024);
-			write(tty_fd, reset_cmd, 4); //send reset command
-			write(tty_fd, send_cmd, 4); //send read data command
-			while(1){
-				ret = ReadMCU(buf2, 70);
-				if(ret < 0) break;
-				buf_index += ret;
-				receive_count += ret;
-				buf2 += ret;
-				if(buf_index == 70){
-					fwrite(buf + 4, 70 - 6, 1, rom);
-					memset(buf, 0, 128 * 1024);
-					buf_index = 0;
-					buf2 = buf;
-					write_count += 64;
-				}
-				if(write_count >= cartridge_len)
-					break;
-			}
-			fflush(rom);
-	  		fclose(rom);
-			if(ret < 0){
-       	                	system(cmd_to_run);
-				printf("error occurs, drop execution stella\n");
-				continue;
-			}
-			printf("Total receive data = %d, write_count = %d\n", receive_count, write_count);
-			if( factory_mode == 0){
-//	            system("/usr/bin/killall stella; /usr/bin/killall r77_front_end; /bin/sleep 1");
-				system("/usr/bin/killall stella; /bin/sleep 1.5");
-				system("/usr/bin/stella /dev/rom.bin&");
-			}
+			runStella(NULL);
+			continue;
 		}
-		system("/bin/sleep 1");
+
+		if (*(buf + 2) != 1) {
+			continue;
+		}
+
+		printf("Cartridge Type: ");
+		switch (*(buf + 3)) {
+			case 0:
+				cartridge_len = 4 * 1024;
+				printf(" 4K\n");
+				break;
+			case 1:
+				cartridge_len = 8 * 1024;
+				printf(" 8K\n");
+				break;
+			case 2:
+				cartridge_len = 16 * 1024;
+				printf("16K\n");
+				break;
+			case 3:
+				cartridge_len = 32 * 1024;
+				printf("32K\n");
+				break;
+			default://can not get correct type
+				runStella(NULL);
+				continue;
+
+				break;
+		}
+
+		int receive_count = 0, buf_index = 0, write_count = 0;
+		unsigned char* buf2 = buf;
+
+		unlink(romfile);
+		sync();
+
+		FILE* rom = fopen(romfile, "w");
+
+		memset(buf, 0, 128 * 1024);
+		write(tty_fd, reset_cmd, 4); //send reset command
+		write(tty_fd, send_cmd, 4); //send read data command
+		while (1) {
+			ret = ReadMCU(buf2, 70);
+			if(ret < 0) break;
+			buf_index += ret;
+			receive_count += ret;
+			buf2 += ret;
+			if(buf_index == 70){
+				fwrite(buf + 4, 70 - 6, 1, rom);
+				memset(buf, 0, 128 * 1024);
+				buf_index = 0;
+				buf2 = buf;
+				write_count += 64;
+			}
+			if(write_count >= cartridge_len)
+				break;
+		}
+
+		fclose(rom);
+		sync();
+
+		if(ret < 0){
+			printf("error occurs, drop execution stella\n");
+
+			unlink(romfile);
+			sync();
+
+			runStella(NULL);
+			continue;
+		}
+
+		printf("Total receive data = %d, write_count = %d\n", receive_count, write_count);
+
+		runStella(romfile);
 	}
+
 	close(tty_fd);
 }
 
